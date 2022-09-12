b15ea181bedc9118d21bd1a211a3914fbcaed8e0
Rubens Santos <rubenssi825@gmail.com>
2022-09-12 18:52:19 -0300

    Add metodos salvar usuario e validar email

--- src/main/java/com/aplication/horadoremedio/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/aplication/horadoremedio/service/impl/UsuarioServiceImpl.java
@@ -3,6 +3,7 @@ package com.aplication.horadoremedio.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.aplication.horadoremedio.exception.RegraNegocioException;
 import com.aplication.horadoremedio.model.entity.Usuario;
@@ -28,14 +29,20 @@ public class UsuarioServiceImpl implements UsuarioService {
 	}
 
 	
-	// antes salvar usuário é preciso validar o email
-	// passando o email do usuario para o metodo validar email
+	// antes de salvar usuário é preciso validar o email
+	// passando o email do usuario para o método validar email
+	// caso não exita email cadastrado salva o usuário na base de dados
+	// retorna uma instância do usuario adastrado
+	// abrir uma tranzação para salvar os dados na base e depois commitar os 
 	@Override
+	@Transactional
 	public Usuario salvarUsuario(Usuario usuario) {
 		validarEmail(usuario.getEmail());
 		return repository.save(usuario);
 	}
-
+	
+	// verificar se existe um usuário cadastrado com o email recebido por parâmetro
+	// caso o email já esteja cadastrado dispara uma exceção com a mensagem email em uso.
 	@Override
 	public void validarEmail(String email) {
 		
