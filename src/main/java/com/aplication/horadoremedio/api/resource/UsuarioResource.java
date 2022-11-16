6128e93a2f55e9ad350d0d4dfa47250866b8e596
rubenspinto <40120074+RubensSI@users.noreply.github.com>
2022-11-16 12:30:56 -0300

    Add endpoint obterUsuario

--- src/main/java/com/aplication/horadoremedio/api/resource/UsuarioResource.java
+++ src/main/java/com/aplication/horadoremedio/api/resource/UsuarioResource.java
@@ -1,8 +1,12 @@
 package com.aplication.horadoremedio.api.resource;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -67,6 +71,17 @@ public class UsuarioResource {
 		} catch (RegraNegocioException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
+	}
+
+	@GetMapping("{id}/usuario")
+	public ResponseEntity obterUsuario(@PathVariable("id") Long id) {
+		Optional<Usuario> usuario = service.obterPorId(id);
+
+		if (!usuario.isPresent()) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+
+		return ResponseEntity.ok(usuario);
 	}
 
 }
